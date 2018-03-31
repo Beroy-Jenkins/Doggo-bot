fc69fef3e233e7da89c305c69f58178b20b21f8e
Beroy <gamarra.eduardo@gmail.com>
2018-03-30 21:06:37 -0300

    final version 01

--- src/dialogs/pesquisar-dialog.js
+++ src/dialogs/pesquisar-dialog.js
@@ -1,7 +1,6 @@
 const builder = require('botbuilder');
 const https = require('https');
 const AzureBingSearch  = require('../services/azure-bing-search');
-//let host = 'api.cognitive.microsoft.com';
 module.exports = [
     (session, results) => {
         builder.Prompts.text(session, 'O que  você gostaria de pesquisar?')
@@ -33,16 +32,13 @@ module.exports = [
           session.send('Consegui encotrar algo: ');
           for (var i = 0; i < value.length; i++ ) {
               var o = value[i];
-              //console.log(" o.name " + o.name ); 
-              //console.log(" o.url " + o.url ); 
+
               const message = 
               `name: **${ o.name }**\n\n`
               + `url: **${ o.url}**\n\n`
-             // + `Tem conteúdo adulto: **${result.adult.isAdultContent}**\n\n`
-             // + `Tem conteúdo racista: **${result.adult.isRacyContent}**\n\n`
-             // + `Tem alguma pessoa na foto: **${result.faces.length}**`            
-  
-             //minhasessao.send("name : %s \n url  : %s",  o.name, o.url  );
+              + `display url: **${o.displayUrl}**\n\n`
+              + `snippet: **${ o.snippet}**\n\n`
+
              session.send(message);
   
   
